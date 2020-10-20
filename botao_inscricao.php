7b83fe9b55151ab4fc8456ac6e5f4e613d7b41b5
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-20 19:38:48 -0200

    Initial commit

---
+++ botao_inscricao.php
@@ -0,0 +1,13 @@
+<?php 
+
+/*
+Plugin Name: botao inscricao
+Plugin URI: www.alexandre.com
+Description: segundo plugin
+Version: 1.0
+Author: alexandre
+Author URI: www.alexandre.com
+License: GLPV2
+Text Domain: segundo plugin
+*/
+
